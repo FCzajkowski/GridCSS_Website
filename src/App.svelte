ee6107f1fcd27f4c76e82807bcc8e834f7348cff
FCzajkowski@proton.me <FCzajkowski@proton.me>
2025-10-05 00:16:16 +0200

    setup

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from './lib/header.svelte';
+
+    const LINK = "https://cdn.jsdelivr.net/gh/FCzajkowski/GridCSS/grid.css";
+    const GITHUB_LINK = "https://github.com/FCzajkowski/GridCSS";
+</script>
+
+<style lang="scss">
+    @use "style";
+</style>
+
+<Header />
---
+++ src/lib/header.svelte
@@ -0,0 +1,19 @@
+<script>
+    import gridImage from '../assets/GridCSS.svg';
+
+    function redirect(website){
+        window.location.href = website;
+    }
+</script>
+<style lang="scss">
+    @use "../style";
+</style>
+
+<header>
+    <img src="{gridImage}" alt="" width="50" height="50">
+    <div>
+        <button on:click="{redirect("/hello")}">Get Started</button>
+        <button on:click="{redirect("https://github.com/FCzajkowski/GridCSS/blob/main/DOCUMENTATION.md")}">Documentation</button>
+        <button on:click="{redirect("https://github.com/FCzajkowski/GridCSS/")}">Github</button>
+    </div>
+</header>
